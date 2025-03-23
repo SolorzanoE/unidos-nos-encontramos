607e305f383ce7bcf7a3c2a43d8eb47c3e0e2f6f
Solorzano <drdslrzn@gmail.com>
2025-03-23 00:47:48 -0600

    Se añadió el proyecto

---
+++ Unidos-nos-Encontramos/Unidos_nos_EncontramosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unidos_nos_EncontramosApp.swift
+//  Unidos-nos-Encontramos
+//
+//  Created by Eduardo Solórzano Santos on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Unidos_nos_EncontramosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
