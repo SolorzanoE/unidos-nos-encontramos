edb95e995a175b14e4cc3fe0e776cc4ccf70b675
Solorzano <drdslrzn@gmail.com>
2025-04-03 17:37:50 -0600

    cambios

--- Unidos-nos-Encontramos/Views/HomeView.swift
+++ Unidos-nos-Encontramos/Views/HomeView.swift
@@ -10,16 +10,15 @@ import MapKit
 
 struct HomeView: View {
     
-    @State private var position: MapCameraPosition = .region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 19.4326, longitude: -99.1332), // CDMX
-            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        )
-    )
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
     
     @StateObject private var homeViewModel = HomeViewModel()
     
     @State private var isPresented: Bool = false
+    
+    @State private var onTrayect: Bool = true
+    
+    @State private var seeTrayect: Bool = true
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -29,8 +28,43 @@ struct HomeView: View {
             }.mapStyle(.standard(pointsOfInterest: .excludingAll))
             
             HStack {
-                JourneyButtonComponent(text: "Iniciar Trayecto") {
-                    isPresented.toggle()
+                
+                if onTrayect {
+                    VStack(alignment: .leading, spacing: 10) {
+                        TextComponent(text: "Trayecto en curso...", style: .subheadline)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.grey500)
+                        
+                        TextComponent(text: "Tiempo estimado: 8 min", style: .body)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.red500)
+                        HStack {
+                            TextComponent(text: "1.7 km - \(formattedCurrentDate())", style: .body)
+                                .fontWeight(.medium)
+                                .foregroundStyle(.grey500)
+                            
+                            Spacer()
+                            
+                            Button {
+                                
+                            } label: {
+                                TextComponent(text: "Finalizar", style: .callout)
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.white500)
+                                    .padding(5)
+                            }.background(.blue500)
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                        }
+                    }.frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                        .background(.white.opacity(0.7))
+                        .background(.ultraThinMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 9))
+                        .clipped()
+                } else {
+                    JourneyButtonComponent(text: "Iniciar Trayecto") {
+                        isPresented.toggle()
+                    }
                 }
                 
                 Button {
@@ -51,9 +85,41 @@ struct HomeView: View {
                 .presentationDetents([.fraction(0.8)])
                 .presentationDragIndicator(.visible)
                 .presentationCornerRadius(25)
+        }.safeAreaInset(edge: .top) {
+            if seeTrayect {
+                HStack(spacing: 50) {
+                    VStack(alignment: .leading, spacing: 10) {
+                        TextComponent(text: "2 trayectos en curso", style: .subheadline)
+                            .fontWeight(.medium)
+                        
+                        TextComponent(text: "Alberto, Eduardo", style: .body)
+                            .fontWeight(.medium)
+                    }.padding(.vertical, 10)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .fontWeight(.bold)
+                            .rotationEffect(.degrees(180))
+                            .foregroundStyle(.grey900)
+                    }
+                }.padding(.horizontal)
+                    .background(.white.opacity(0.5))
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing)
+            }
         }
     }
 }
+
+func formattedCurrentDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"  // Puedes cambiar este formato según lo que necesites
+        return formatter.string(from: Date())
+    }
 
 #Preview {
     HomeView()
